bcd5d1a1a667e73ba9ba786c07484854db6227c6
Rohith Reddy <95706806+rohithreddy2001@users.noreply.github.com>
2023-02-11 18:30:56 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -6,21 +6,6 @@ import Lock from './pages/Homepage/Lock';
 
 function App() {
 
-  const [whatsAppLock,setWhatsAppLock] = useState(false)
-
-    const lockHandler = () => {
-        console.log("WhatsApp Locked")
-        setWhatsAppLock(!whatsAppLock)
-
-    }
-
-    const [unLock,setUnLock] = useState(false)
-
-    const unLockHandler = () => {
-        console.log("unlock")
-        setWhatsAppLock(!whatsAppLock)
-    }
-
   return (
     <div className="App">
       <Homepage />
